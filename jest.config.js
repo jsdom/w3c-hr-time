a01031dba1f7f52c27099a47d59833bd2fa51275
Domenic Denicola <d@domenic.me>
2022-10-16 16:27:57 +0900

    Deprecate the package

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-module.exports = {
-  coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/test/"],
-  verbose: true
-};
--- test/clock-drift.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const FACTOR = require("./utils/slow-down-clock");
-
-require("./primitives/global-monotonic-clock")(FACTOR);
-require("./primitives/performance-common")();
